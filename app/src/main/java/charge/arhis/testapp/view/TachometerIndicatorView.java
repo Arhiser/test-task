ccaaf43d7807a94bd34e7acfbdb32292f2224878
Sergey Arkhipov <arhiser@yandex.ru>
2017-02-25 12:35:46 +0300

    Improvements

--- app/src/main/java/charge/arhis/testapp/view/TachometerIndicatorView.java
+++ app/src/main/java/charge/arhis/testapp/view/TachometerIndicatorView.java
@@ -7,6 +7,8 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -23,7 +25,7 @@ public class TachometerIndicatorView extends View {
     private int[] paddings = new int[4];
     private Rect rect = new Rect();
     private RectF rectF = new RectF();
-    private int gaugeColor = 0xff303030;
+    private int gaugeBackgroundColor = 0xff303030;
     private int pointerColor = 0xffff3030;
     private int pinColor = 0xff808080;
     private int textColor = 0xff50ff50;
@@ -38,10 +40,21 @@ public class TachometerIndicatorView extends View {
 
     float[] pointerPath = new float[] {
             0, -0.9f,
-            -0.03f, 0.1f,
-            -0.03f, 0.1f,
-            0.03f, 0.1f,
-            0.03f, 0.1f,
+            -0.02f, -0.3f,
+
+            -0.02f, -0.3f,
+            -0.03f, 0.18f,
+
+            -0.03f, 0.18f,
+            0.00f, 0.173f,
+
+            0.00f, 0.173f,
+            0.03f, 0.18f,
+
+            0.03f, 0.18f,
+            0.02f, -0.3f,
+
+            0.02f, -0.3f,
             0, -0.9f
     };
 
@@ -61,22 +74,16 @@ public class TachometerIndicatorView extends View {
     }
 
     private void init(AttributeSet attrs) {
-
         if (attrs != null) {
-            int[] attributes = new int[] {android.R.attr.paddingLeft, android.R.attr.paddingTop, android.R.attr.paddingRight, android.R.attr.paddingBottom, android.R.attr.padding};
+            int[] attributes = new int[] {android.R.attr.paddingLeft, android.R.attr.paddingTop, android.R.attr.paddingRight, android.R.attr.paddingBottom};
             TypedArray arr = getContext().obtainStyledAttributes(attrs, attributes);
-
-            int padding = arr.getDimensionPixelOffset(4, -1);
-            if (padding >=0 ) {
-                for (int i = 0; i < 4; i++) {
-                    paddings[i] = padding;
-                }
-            } else {
+            try {
                 for (int i = 0; i < 4; i++) {
                     paddings[i] = arr.getDimensionPixelOffset(i, 0);
                 }
+            } finally {
+                arr.recycle();
             }
-            arr.recycle();
 
             TypedArray a = getContext().getTheme().obtainStyledAttributes(
                     attrs,
@@ -88,7 +95,6 @@ public class TachometerIndicatorView extends View {
             } finally {
                 a.recycle();
             }
-
         }
 
         paint.setAntiAlias(true);
@@ -98,10 +104,7 @@ public class TachometerIndicatorView extends View {
     public void draw(Canvas canvas) {
         super.draw(canvas);
 
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(0xffff0000);
-        paint.setStrokeWidth(3);
-
+        //calculate diagram size on canvas
         int size = Math.min(getWidth() - paddings[0] - paddings[2], getHeight() - paddings[1] - paddings[3]);
 
         int totalXPadding = paddings[0] + (getWidth() - paddings[0] - paddings[2] - size) / 2;
@@ -112,12 +115,16 @@ public class TachometerIndicatorView extends View {
         int restoreTo = canvas.save();
 
         canvas.clipRect(rectF);
+
+        //draw background
         paint.setStyle(Paint.Style.FILL);
-        paint.setColor(gaugeColor);
+        paint.setColor(gaugeBackgroundColor);
         canvas.drawOval(rectF, paint);
 
         canvas.save();
         canvas.translate(getWidth() / 2, getHeight() / 2);
+
+        //draw text labels
         paint.setColor(textColor);
         paint.setTextSize(50f);
         int textXShift = (int)paint.measureText("8")/2;
@@ -137,28 +144,25 @@ public class TachometerIndicatorView extends View {
         labelY += paint.getFontMetrics().descent - paint.getFontMetrics().ascent;
         canvas.drawText("x1000", labelX, labelY, paint);
 
-        canvas.restore();
-
-        canvas.save();
-
-        canvas.translate(getWidth() / 2, getHeight() / 2);
         canvas.scale(size / 2, size / 2);
         canvas.rotate(180);
 
         paint.setColor(pointerColor);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(0.02f);
-
+        //draw pointer arrow
         canvas.save();
         canvas.rotate((rpm/gaugeMaxValue) * 270);
         canvas.drawLines(pointerPath, paint);
         canvas.restore();
 
+        //draw arrow pivot
         paint.setColor(pinColor);
         paint.setStyle(Paint.Style.FILL);
         rectF.set(.05f, .05f, -.05f, -.05f);
         canvas.drawCircle(0, 0, 0.06f, paint);
 
+        //draw gauge marks
         paint.setColor(pointerColor);
         paint.setStyle(Paint.Style.STROKE);
         canvas.save();
@@ -211,5 +215,58 @@ public class TachometerIndicatorView extends View {
             }
         }
         return true;
+    }
+
+    public Parcelable onSaveInstanceState() {
+        if (animator != null && animator.isRunning()) {
+            animator.end();
+        }
+        Parcelable superState = super.onSaveInstanceState();
+        return new State(superState).setRpm(rpm);
+    }
+
+    public void onRestoreInstanceState(Parcelable savedState) {
+        State state = (State) savedState;
+        super.onRestoreInstanceState(state.getSuperState());
+        setRpm(state.rpm);
+    }
+
+    private static class State extends BaseSavedState {
+
+        float rpm;
+
+        public State(Parcel source) {
+            super(source);
+            rpm = source.readFloat();
+        }
+
+        public State(Parcelable superState) {
+            super(superState);
+        }
+
+        public State setRpm(float rpm) {
+            this.rpm = rpm;
+            return this;
+        }
+
+        public float getRpm() {
+            return rpm;
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeFloat(rpm);
+        }
+
+        public static final Parcelable.Creator<State> CREATOR = new Parcelable.Creator<State>() {
+            public State createFromParcel(Parcel in) {
+                return new State(in);
+            }
+
+            public State[] newArray(int size) {
+                return new State[size];
+            }
+        };
     }
 }
